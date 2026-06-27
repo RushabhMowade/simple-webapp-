53245b1606af26cba35ec3f8a289da7cb8960171
Rushabh Mowade <rushabhmwd@gmail.com>
2026-06-27 19:44:52 +0530

    Search Product

--- src/main/java/com/Rush/Ecom/controller/ProductController.java
+++ src/main/java/com/Rush/Ecom/controller/ProductController.java
@@ -89,9 +89,10 @@ public class ProductController {
         }
         else return new ResponseEntity<>("Product not found",HttpStatus.NOT_FOUND);
 }
-@GetMapping("/product/search")
+@GetMapping("/products/search")
     public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
 
+        System.out.println("Searching with " + keyword);
         List<Product> products = service.searchProducts(keyword);
         return new ResponseEntity<>(products,HttpStatus.OK);
 }
